fd8edeee5afb3faaac3dba3442a9512c20364e0c
Şahin Aydoğan <43421169+asphyxiatedd@users.noreply.github.com>
2023-11-22 19:31:37 +0300

    Basit Usul Hesap Makinesi

--- Main.java
+++ Main.java
@@ -1,46 +1,42 @@
-import java.math.MathContext;
+import javax.swing.*;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        //Meyveler ve KG Fiyatları
-        //Armut : 2,14 TL Elma : 3,67 TL Domates : 1,11 TL Muz: 0,95 TL Patlıcan : 5,00 TL
-        //Örnek Çıktı;
-        //Armut Kaç Kilo ? :0
-        //Elma Kaç Kilo ? :1
-        //Domates Kaç Kilo ? :1
-        //Muz Kaç Kilo ? :2
-        //Patlıcan Kaç Kilo ? :3
-        //Toplam Tutar : 21.68 TL
-        double elmakg,muzkg,domateskg,armutkg,patlicankg;
-        double muz = 0.95;
-        double domates=1.11;
-        double armut=2.14;
-        double patlican=5.00;
-        double elma=3.67;
+        int n1, n2, select, sonuc;
         Scanner input = new Scanner(System.in);
-        System.out.print("Kac Kg Elma ? ");
-        elmakg= input.nextInt();
-        System.out.print("Kac Kg Muz?  ");
-         muzkg= input.nextInt();
-        System.out.print("Kac Kg Domates? ");
-        domateskg= input.nextInt();
-        System.out.print("Kac Kg Armut?");
-        armutkg= input.nextInt();
-        System.out.print("Kac Kg Patlican?");
-        patlicankg= input.nextInt();
+        System.out.print("ilk Sayiyi Giriniz: ");
+        n1 = input.nextInt();
+        System.out.print("Ikinci Sayiyi Giriniz: ");
+        n2 = input.nextInt();
+        System.out.println("Yapmak istediginiz islemi secin");
+        System.out.println("Toplama icin 1\nCıkartma İcin 2\nBolme İcin 3\nCarpma İcin 4");
+        select = input.nextInt();
 
-         double total=domateskg*domates+ armutkg*armut+elmakg*elma+patlicankg*patlican+muzkg*muz;
-        System.out.println("Toplam Tutar : " +total );
+        switch (select) {
+            case 1:
+                sonuc = n2 + n1;
+                System.out.println("Sonuc: " + sonuc);
+                break;
+            case 2:
+                sonuc = n1 - n2;
+                System.out.println("Sonuc: " + sonuc);
+                break;
+            case 3:
+                if (n2 != 0) {
+                    sonuc = n1 / n2;
+                    System.out.println("Sonuc: " + sonuc);
+                } else {
+                    System.out.println("Sıfıra Bölüm Yapamazsınız");
+                }
+                break;
+            case 4:
+                sonuc = n1 * n2;
+                System.out.println("Sonuc: " + sonuc);
+                break;
+            default:
+                System.out.println("Hatalı Giriş Yaptınız! Tekrar Deneyiniz");
 
-
-
-
-
-
-
-
-
-
-            }
+        }
+    }
 }
