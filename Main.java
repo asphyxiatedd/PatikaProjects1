5f8d3ee5fe3c5f5bf85518d79c898beac8715ae1
Şahin Aydoğan <43421169+asphyxiatedd@users.noreply.github.com>
2023-11-22 21:51:09 +0300

    küçükten büyüğe sıralama

--- Main.java
+++ Main.java
@@ -3,23 +3,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int heat;
+        int a, b, c;
         Scanner input = new Scanner(System.in);
-        System.out.println("Hava kaç derece?");
-        heat = input.nextInt();
-        if (heat < 5) {
-            System.out.println("Kayak Yapabilirsiniz");
-        } else if (heat >= 5 && heat < 15) {
-            System.out.println("Sinemaya Gidebilirsiniz");
-        } else if (heat >= 15 && heat <= 25) {
-            System.out.println("Piknik yapabilirsiniz");
-        } else if (heat > 25) {
-            System.out.println("Yüzemeye Gidebilirsiniz");
 
+        System.out.print("1. Sayıyı Giriniz: ");
+        a = input.nextInt();
+        System.out.print("2. Sayıyı Giriniz: ");
+        b = input.nextInt();
+        System.out.print("3. Sayıyı Giriniz: ");
+        c = input.nextInt();
+
+        if (a <= b && b <= c) {
+            System.out.println("Sıralama: " + a + " <= " + b + " <= " + c);
+        } else if (a <= c && c <= b) {
+            System.out.println("Sıralama: " + a + " <= " + c + " <= " + b);
+        } else if (b <= a && a <= c) {
+            System.out.println("Sıralama: " + b + " <= " + a + " <= " + c);
+        } else if (b <= c && c <= a) {
+            System.out.println("Sıralama: " + b + " <= " + c + " <= " + a);
+        } else if (c <= a && a <= b) {
+            System.out.println("Sıralama: " + c + " <= " + a + " <= " + b);
+        } else {
+            System.out.println("Sıralama: " + c + " <= " + b + " <= " + a);
         }
     }
 
-}
+        }
+
+
+
+
 
 
 
