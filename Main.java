f169668e3a9851ab38ffe491d131deac2537042b
Şahin Aydoğan <43421169+asphyxiatedd@users.noreply.github.com>
2023-11-22 03:16:23 +0300

    Manav kasa Programi

--- Main.java
+++ Main.java
@@ -3,20 +3,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Kilo (kg) / Boy(m) * Boy(m)
-        //Çıktısı
-        //Lütfen boyunuzu (metre cinsinde) giriniz : 1,72
-        //Lütfen kilonuzu giriniz : 105
-        //Vücut Kitle İndeksiniz : 35.49215792320173
-        double kg ;
-        double m;
+        //Meyveler ve KG Fiyatları
+        //Armut : 2,14 TL Elma : 3,67 TL Domates : 1,11 TL Muz: 0,95 TL Patlıcan : 5,00 TL
+        //Örnek Çıktı;
+        //Armut Kaç Kilo ? :0
+        //Elma Kaç Kilo ? :1
+        //Domates Kaç Kilo ? :1
+        //Muz Kaç Kilo ? :2
+        //Patlıcan Kaç Kilo ? :3
+        //Toplam Tutar : 21.68 TL
+        double elmakg,muzkg,domateskg,armutkg,patlicankg;
+        double muz = 0.95;
+        double domates=1.11;
+        double armut=2.14;
+        double patlican=5.00;
+        double elma=3.67;
         Scanner input = new Scanner(System.in);
-        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz: ");
-         m = input.nextDouble();
-        System.out.print("Lütfen kilonuzu giriniz: ");
-         kg= input.nextInt();
-         double endeks = kg / (m*m);
-        System.out.println("Vucut Kitle Endeksiniz : " + endeks);
+        System.out.print("Kac Kg Elma ? ");
+        elmakg= input.nextInt();
+        System.out.print("Kac Kg Muz?  ");
+         muzkg= input.nextInt();
+        System.out.print("Kac Kg Domates? ");
+        domateskg= input.nextInt();
+        System.out.print("Kac Kg Armut?");
+        armutkg= input.nextInt();
+        System.out.print("Kac Kg Patlican?");
+        patlicankg= input.nextInt();
+
+         double total=domateskg*domates+ armutkg*armut+elmakg*elma+patlicankg*patlican+muzkg*muz;
+        System.out.println("Toplam Tutar : " +total );
 
 
 
